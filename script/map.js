7736b690da8775922b406b1f00da6fd4a9d21f6a
Mariah Malczewska <mariahmalcz@gmail.com>
2026-07-24 14:32:51 -0700

    Add map marker and related

--- script/map.js
+++ script/map.js
@@ -1,3 +1,5 @@
+"user strict";
+
 const victoriaCoordinates = [48.428813, -123.363040];
 
 let map = L.map('map').setView(victoriaCoordinates, 13); // or var to consistant w leaflet instructions
@@ -9,26 +11,35 @@ L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
 // let marker = L.marker([48.4, -123.4]).addTo(map); //Map marker - default
 //for custom need a png for image, and a png for shadow
 var hotelIcon = L.icon({
-    iconUrl: '..\img\icon-park-solid--hotel.svg', //doesnt appear to handle svg
-    // shadowUrl: '..\img\icon-park-solid--hotel.svg',
+    iconUrl: '../img/hotelIcon-32.png', //doesnt appear to handle svg
+    shadowUrl: '../img/hotelIconShadow-32x11.png',
 
-    iconSize:     [38, 95], // size of the icon
-    shadowSize:   [50, 64], // size of the shadow
-    iconAnchor:   [22, 94], // point of the icon which will correspond to marker's location
-    shadowAnchor: [4, 62],  // the same for the shadow
+    iconSize:     [32, 32], // size of the icon
+    shadowSize:   [32, 11], // size of the shadow
+    iconAnchor:   [0, 0], // point of the icon which will correspond to marker's location
+    shadowAnchor: [0, -30],  // the same for the shadow
     popupAnchor:  [-3, -76] // point from which the popup should open relative to the iconAnchor
 });
-let marker = L.marker([48.4, -123.4], {icon: hotelIcon}).addTo(map); //Map marker - default
+let marker = L.marker([48.43, -123.36], {icon: hotelIcon}).addTo(map); //Map marker - default
 
 
 // marker.bindPopup("<b>Hello world!</b><br>I am a popup."); //simple marker note (add openPopup() to open on load)
 
 let popup = L.popup();
 function onMapClick(e) {
+    console.log(e);
+        let lat = Number( e.latlng.lat.toFixed(3) );
+        let lng = Number( e.latlng.lng.toFixed(3) );
+        let coords = [ lat, lng ];
+        console.log( coords );
+    map.panTo( coords ); //not centering properly; but variable is correct type...
+
     popup //this structure is good for when need to attach more than a simple note, eg. weather info
-        .setLatLng([51.513, -0.09]) //lol jumps to london
-        .setContent("You clicked the map at " + e.latlng)
+        // .setLatLng(e.latlng) //not jumping :( )
+        .setLatLng( coords ) //not jumping :( )        
+        .setContent("You clicked the map at " + coords )
         .openOn(map);
+        ;
 }
 
 map.on('click', onMapClick);
