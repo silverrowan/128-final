77633a08daab0edf0c4e772b50d8e0d5d42c25a9
Mariah Malczewska <mariahmalcz@gmail.com>
2026-07-24 13:51:20 -0700

    Create room and hotel objects

---
+++ script/rooms.js
@@ -0,0 +1,138 @@
+"use strict";
+
+class Hotel {
+    #id;
+    #name;
+    #city;
+    #country;
+    #lat;
+    #long;
+    #rating;
+    #description;
+    #image;
+
+    //constructors
+    constructor( id, name, city, country, lat, long, rating, description, image ) {
+        this.#id=id;
+        this.#name=name;
+        this.#city=city;
+        this.#country=country;
+        this.#lat=lat;
+        this.#long=long;
+        this.#rating=rating;
+        this.#description=description;
+        this.#image=image
+    }
+
+    //setters & getters
+    get id() { return this.#id; }
+    set id(  n) { this.#id = n; }
+
+    get name() { return this.#name; }
+    set name( n ) { this.#name = n; }
+
+    get city() { return this.#city; }
+    set city( n ) { this.#city = n; }
+
+    get country() { return this.#country; }
+    set country(  n) { this.#country = n; }
+
+    get lat() { return this.#lat; }
+    set lat( n ) { this.#lat = n; }
+
+    get long() { return this.#long; }
+    set long( n ) { this.#long = n; }
+
+    get rating() { return this.#rating; }
+    set rating(  n) { this.#rating = n; }
+
+    get description() { return this.#description; }
+    set description( n ) { this.#description = n; }
+
+    get image() { return this.#image; }
+    set image( n ) { this.#image = n; }
+
+    //additional obj functions
+}
+
+class Room {
+    #id;
+    #hotelId;
+    #name;
+    #type;
+    #beds;
+    #maxGuests;
+    #pricePerNight;
+    #rating;
+    #available;
+    #image;
+
+    //constructors
+    constructor( id, hotelId, name, type, beds, maxGuests, pricePerNight, rating, available, image ) {
+        this.#id=id;
+        this.#hotelId=hotelId;
+        this.#name=name;
+        this.#type=type;
+        this.#beds=beds;
+        this.#maxGuests=maxGuests;
+        this.#pricePerNight=pricePerNight;
+        this.#rating=rating;
+        this.#available=available;
+        this.#image=image
+    }
+
+    //setters & getters
+    get id() { return this.#id; }
+    set id(  n) { this.#id = n; }
+
+    get hotelId() { return this.#hotelId; }
+    set hotelId( n ) { this.#hotelId = n; }
+
+    get name() { return this.#name; }
+    set name( n ) { this.#name = n; }
+
+    get type() { return this.#type; }
+    set type( n ) { this.#type = n; }
+
+    get beds() { return this.#beds; }
+    set beds(  n) { this.#beds = n; }
+
+    get maxGuests() { return this.#maxGuests; }
+    set maxGuests( n ) { this.#maxGuests = n; }
+
+    get pricePerNight() { return this.#pricePerNight; }
+    set pricePerNight( n ) { this.#pricePerNight = n; }
+
+    get rating() { return this.#rating; }
+    set rating(  n) { this.#rating = n; }
+
+    get available() { return this.#available; }
+    set available( n ) { this.#available = n; }
+
+    get image() { return this.#image; }
+    set image( n ) { this.#image = n; }
+
+    //additional obj functions    
+}
+
+
+
+const getHotelInfo = () => {
+    const hotelLocation = "../public/hotels.json";
+
+    //get all hotel data, insert it into/parse it into object
+    return fetch( hotelLocation )
+        .then ( response => response.json() )
+        .then ( data => data )
+        .catch ( error => console.log( error.message ));
+}
+
+const getRoomInfo = () => {
+        const roomLocation = "../public/rooms.json";
+
+    //get all room data, insert it into/parse it into object
+    return fetch( roomLocation )
+        .then ( response => response.json() )
+        .then ( data => data )
+        .catch ( error => console.log( error.message ));
+}
